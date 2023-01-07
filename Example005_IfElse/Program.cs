b3541ae8da2c053b9ee3cc5e43c582c8b9bf0613
Damir <massalimov.d@gmail.com>
2023-01-07 22:13:54 +0600

    Example005_IfElse project

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Oh, this is MASHA!");
+}
+else
+{
+    Console.Write("Hello ");
+    Console.WriteLine(username);   
+}
